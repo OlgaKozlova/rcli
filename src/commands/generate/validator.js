4b9d1d1c945f6c01cb1ba2a1679e1688dc67cd4f
Olga Kozlova <olgakozlova@coherentsolutions.com>
2017-06-29 13:13:58 +0300

    WIP

--- src/commands/generate/validator.js
+++ src/commands/generate/validator.js
@@ -1,6 +1,9 @@
 const fs = require('fs-extra');
 const path = require('path');
 
+const allowedTemplateTypes = ['file', 'string'];
+const allowedActions = ['create', 'appendBottom'];
+
 const getErrorResult = errors => ({
     isValid: false,
     errors,
@@ -36,10 +39,36 @@ function checkTemplatesExistency(parameters, options, conf) {
         : [];
 }
 
+function checkActions(parameters, options, conf) {
+    const bundle = conf.bundles[parameters[0]];
+
+    return bundle
+        ? Object.keys(bundle)
+            .filter(key => !allowedActions.includes(bundle[key].action))
+            .map(key => `Invalid template type found in configuration: ${bundle[key].template}.
+             Check "${key} configuration for "${parameters[0]}" bundle.
+             Only ${allowedActions.join(',')} templateTypes are allowed"`)
+        : [];
+}
+
+function checkTemplateType(parameters, options, conf) {
+    const bundle = conf.bundles[parameters[0]];
+
+    return bundle
+        ? Object.keys(bundle)
+            .filter(key => !allowedTemplateTypes.includes(bundle[key].templateType))
+            .map(key => `Invalid template type found in configuration: ${bundle[key].template}.
+             Check "${key} configuration for "${parameters[0]}" bundle.
+             Only ${allowedTemplateTypes.join(',')} templateTypes are allowed"`)
+        : [];
+}
+
 module.exports = function validate(parameters, options, conf) {
     const errors = [
         checkParamsCount.bind(null, parameters),
         checkBundle.bind(null, parameters, conf),
+        checkTemplateType.bind(null, parameters, conf),
+        checkActions.bind(null, parameters, conf),
         checkTemplatesExistency.bind(null, parameters, options, conf),
     ].reduce((result, current) => result.concat(current()), []);
 
